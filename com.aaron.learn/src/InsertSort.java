9f341679eceb56fe7a263914896f9b6265221b93
Aaron <17629093947@163.com>
2019-05-02 23:32:36 +0800

    练习 Idea 里面 git 的使用

---
+++ com.aaron.learn/src/InsertSort.java
@@ -0,0 +1,5 @@
+public class InsertSort {
+    public static void main(String[] args) {
+        System.out.println("怎么样，我搞懂了哈");
+    }
+}
---
+++ com.aaron.learn/src/QuickSort.java
@@ -0,0 +1,5 @@
+public class QuickSort {
+    public static void main(String[] args) {
+        System.out.println("这是快速排序");
+    }
+}
